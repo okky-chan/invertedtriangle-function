08c57ca487b568f1ee7487002f0f7826e95ce2a3
Okky Dwi Triferyawan <61794853+okky-chan@users.noreply.github.com>
2021-12-27 14:17:13 +0700

    Add files via upload

---
+++ triangle.py
@@ -0,0 +1,12 @@
+def triangle(row):
+    for i in range(row,0,-1):
+        for j in range(row-i):
+            print(' ', end='')
+    
+        for j in range(2*i-1):
+            print('*',end='')
+        print()
+
+row = int(input('Input number: '))
+
+triangle(row)
